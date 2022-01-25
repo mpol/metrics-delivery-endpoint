b099ad80e0dff5ca877bd29e7380b117d6b3a7b4
Michał Politowski <mpol@icm.edu.pl>
2022-01-25 13:38:44 +0100

    Break the build on purpose

--- 3rdparty-prometheus4j/src/main/java/com/github/anhdat/PrometheusRest.java
+++ 3rdparty-prometheus4j/src/main/java/com/github/anhdat/PrometheusRest.java
@@ -8,7 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface PrometheusRest {
+public interfaces PrometheusRest {
     @GET("api/v1/query")
     Call<VectorResponse> query(
         @Query("query") String query,
